0908c689f41595eb97412e03d49ea73a469a0d08
Fatrai1 <fatrai.cb@gmail.com>
2021-05-03 14:55:35 +0200

    kész

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
-'use strict'
+'use strict';
+
+const sendButton = $('#send');
+
+sendButton.click(() => {
+
+    let name = $("#name").val();
+    let name = $("#email").val();
+    let name = $("#subject").val();
+    let name = $("#message").val(); 
+
+    $.ajax({
+        url: 'sendmail.php',
+        method: 'POST',
+        data: { name: name, email: email, subject: subject, description: description},
+        dataType: 'html',
+    }).done( function(data) {
+
+        // $('.result').html(data);     Ez ugyanazt csijálja mint az alattalévő sor, csak jquery-ben. sokkal rövidebb, lehet a kódokat vegyíteni
+
+        document.querySelector('.result').textContent = data;
+    });
+});
 
